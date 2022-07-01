f22e3379a4228372baa625f85f2297c868857b67
Romain MAZIERE <maz@rmaziere.fr>
2022-07-01 16:04:03 +0200

    Update post_initial.groovy

--- post_initial.groovy
+++ post_initial.groovy
@@ -1,8 +1,8 @@
   post {
        success {
-         slackSend (color: '#00FF00', message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+         slackSend (color: '#00FF00', message: "MAZ - SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
          }
       failure {
-            slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+            slackSend (color: '#FF0000', message: "MAZ - FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
           }   
     }
